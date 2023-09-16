e107711ecff9e6cde5b11b1f33dcad27f9d52cbe
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-09-16 14:26:47 -0300

    add novas functions para busca de dados

--- src/api/gitHub.ts
+++ src/api/gitHub.ts
@@ -1,6 +1,6 @@
-import { ResponseUser } from "@/types";
+import {  ErrorResponseUser, ResponseRepos, ResponseUser, ResponseUserNotFound} from "@/types";
 
-const BASE_URL = "https://api.github.com/";
+export const BASE_URL = "https://api.github.com";
 
 async function fetcher<TypeResponse>(url: string, messageError: string) {
   const response = await fetch(url, {
@@ -19,4 +19,38 @@ async function fetcher<TypeResponse>(url: string, messageError: string) {
 export async function getUser(username: string) {
   const url = `${BASE_URL}/users/${username}`;
   return await fetcher<ResponseUser>(url, "Failed to fetch datas user");
+}
+
+export async function fetcherUser(url: string, username: string) {
+  const response = await fetch(url, {
+    cache: "default"
+  });
+
+  if (!response.ok) {
+    const responseError: ResponseUserNotFound = await response.json();
+    const error: ErrorResponseUser = {
+      message: "Failed to fetch datas user", info: responseError.message,
+      name: ""
+    };
+    error.info = responseError.message;
+    throw error;
+  }
+
+  const result: ResponseUser = await response.json();
+
+  return result;
+}
+
+export async function fetcherRepos(url: string, username: string) {
+  const response = await fetch(url, {
+    cache: "default"
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed fetch repositories from user");
+  }
+
+  const result: ResponseRepos = await response.json();
+
+  return result;
 }
