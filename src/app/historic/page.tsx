04aecc29f73f231ae392d8bf9a0bf11368fc2623
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-09-17 09:26:45 -0300

    add nova estrutura html

--- src/app/historic/page.tsx
+++ src/app/historic/page.tsx
@@ -1,5 +1,6 @@
 import Historic from "@/components/Historic";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "History Searchs",
@@ -8,13 +9,28 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <main>
-      <h1>History Searchs</h1>
-      <p>
-        Below are all previously searched terms, ordered by date and time by
-        most recent
-      </p>
-      <Historic />
-    </main>
+    <div>
+      <header>
+        <nav>
+          <Link
+            href="/"
+            target="_self"
+            rel="next"
+            title="Go Back Home"
+            aria-label="Go Back Home"
+          >
+            Home
+          </Link>
+        </nav>
+        <h1>History Searchs</h1>
+        <p>
+          Below are all previously searched terms, ordered by date and time by
+          most recent
+        </p>
+      </header>
+      <main>
+        <Historic />
+      </main>
+    </div>
   );
 }
