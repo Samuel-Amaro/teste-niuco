41284b0df80279b705d3d5baceceec2b668880dc
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-09-17 13:59:56 -0300

    nova estrutura do page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,58 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Form from "@/components/Form";
+import Searcher from "@/components/Searcher";
+import { SearchParams } from "@/types";
+import Link from "next/link";
+import styles from "./page.module.css";
 
-export default function Home() {
+export default function Home({ searchParams }: { searchParams: SearchParams }) {
+  const pageInd = searchParams.page ? searchParams.page : "1";
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.containerMain}>
+      <header className={styles.header}>
+        <nav className={styles.nav}>
+          <ul className={styles.list}>
+            <li>
+              <Link
+                href="/"
+                target="_self"
+                rel="next"
+                title="Page Home"
+                aria-label="Home"
+                className={styles.link}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/historic"
+                target="_self"
+                rel="next"
+                title="Historic"
+                aria-label="Historic"
+                className={styles.link}
+              >
+                Historic
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.containerText}>
+          <h1 className={styles.title}>Github username searcher</h1>
+          <p className={styles.description}>Discover data from a GitHub user</p>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      </header>
+      <main>
+        <Form initialValue={searchParams.q} />
+        <Searcher
+          username={searchParams.q ? searchParams.q : "samuel-amaro"}
+          pageIndex={
+            !isNaN(parseInt(pageInd)) && parseInt(pageInd) >= 1
+              ? parseInt(pageInd)
+              : 1
+          }
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+    </div>
+  );
 }
