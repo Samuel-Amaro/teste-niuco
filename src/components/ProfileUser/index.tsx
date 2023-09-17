8af9a88bab8624e1a280b10acbdc2c4c1eded106
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-09-17 13:58:38 -0300

    add class css componente

--- src/components/ProfileUser/index.tsx
+++ src/components/ProfileUser/index.tsx
@@ -2,10 +2,11 @@ import { ResponseUser } from "@/types";
 import { shimer, toBase64 } from "@/utils";
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./styles.module.css";
 
 export default function ProfileUser({ datas }: { datas: ResponseUser }) {
   return (
-    <div>
+    <div className={styles.container}>
       {datas.avatar_url && (
         <Image
           src={datas.avatar_url}
@@ -16,11 +17,12 @@ export default function ProfileUser({ datas }: { datas: ResponseUser }) {
           blurDataURL={`data:image/svg+xml;base64,${toBase64(
             shimer(360, 360),
           )}`}
+          className={styles.image}
         />
       )}
       <div>
-        <div>
-          {datas.name && <h2>{datas.name}</h2>}
+        <div className={styles.containerText}>
+          {datas.name && <h2 className={styles.name}>{datas.name}</h2>}
           {datas.login && datas.html_url && (
             <Link
               href={datas.html_url}
@@ -28,55 +30,78 @@ export default function ProfileUser({ datas }: { datas: ResponseUser }) {
               rel="external"
               title="Url Profile"
               aria-label="Url Profile"
+              className={styles.link}
             >{`@${datas.login}`}</Link>
           )}
         </div>
-        <p>{datas.bio ? datas.bio : "This Profile has no bio"}</p>
-        <div>
+        <p className={styles.description}>
+          {datas.bio ? datas.bio : "This Profile has no bio"}
+        </p>
+        <div className={styles.containerSocial}>
           {datas.public_repos && (
-            <p>
-              <span>Repos</span>
-              <span>{datas.public_repos}</span>
+            <p className={styles.socialItem}>
+              <span className={styles.socialItemInfo}>Repos</span>
+              <span className={styles.socialItemValue}>
+                {datas.public_repos}
+              </span>
             </p>
           )}
           {datas.followers && (
-            <p>
-              <span>Followers</span>
-              <span>{datas.followers}</span>
+            <p className={styles.socialItem}>
+              <span className={styles.socialItemInfo}>Followers</span>
+              <span className={styles.socialItemValue}>{datas.followers}</span>
             </p>
           )}
           {datas.following && (
-            <p>
-              <span>Following</span>
-              <span>{datas.following}</span>
+            <p className={styles.socialItem}>
+              <span className={styles.socialItemInfo}>Following</span>
+              <span className={styles.socialItemValue}>{datas.following}</span>
             </p>
           )}
         </div>
-        <div>
-          {datas.location && <p>{datas.location}</p>}
+        <div className={styles.datas}>
+          {datas.location && (
+            <div className={styles.containerData}>
+              <p className={styles.containerDataInfo}>Location</p>
+              <p className={styles.containerDataValue}>{datas.location}</p>
+            </div>
+          )}
           {datas.blog && (
-            <Link
-              href={datas.blog}
-              target="_blank"
-              rel="external"
-              title="Url Blog"
-              aria-label="Url Blog"
-            >
-              @Blog
-            </Link>
+            <div className={styles.containerData}>
+              <p className={styles.containerDataInfo}>Blog</p>
+              <Link
+                href={datas.blog}
+                target="_blank"
+                rel="external"
+                title="Url Blog"
+                aria-label="Url Blog"
+                className={styles.link}
+              >
+                @Blog
+              </Link>
+            </div>
           )}
           {datas.email && (
-            <Link
-              href={`mailto:${datas.email}`}
-              target="_blank"
-              rel="external"
-              title="Email"
-              aria-label="Email"
-            >
-              {datas.email}
-            </Link>
+            <div className={styles.containerData}>
+              <p className={styles.containerDataInfo}>Email</p>
+              <Link
+                href={`mailto:${datas.email}`}
+                target="_blank"
+                rel="external"
+                title="Email"
+                aria-label="Email"
+                className={styles.link}
+              >
+                {datas.email}
+              </Link>
+            </div>
           )}
-          {datas.company && <p>{datas.company}</p>}
+          {datas.company && (
+            <div className={styles.containerData}>
+              <p className={styles.containerDataInfo}>Company</p>
+              <p className={styles.containerDataValue}>{datas.company}</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
