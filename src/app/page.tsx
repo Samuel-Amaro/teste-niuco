75f5d6b68942e9262d248955a33691126f3f91d9
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-09-17 14:30:35 -0300

    add componente Nav layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,40 +3,13 @@ import Searcher from "@/components/Searcher";
 import { SearchParams } from "@/types";
 import Link from "next/link";
 import styles from "./page.module.css";
+import Nav from "@/components/Nav";
 
 export default function Home({ searchParams }: { searchParams: SearchParams }) {
   const pageInd = searchParams.page ? searchParams.page : "1";
   return (
     <div className={styles.containerMain}>
       <header className={styles.header}>
-        <nav className={styles.nav}>
-          <ul className={styles.list}>
-            <li>
-              <Link
-                href="/"
-                target="_self"
-                rel="next"
-                title="Page Home"
-                aria-label="Home"
-                className={styles.link}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/historic"
-                target="_self"
-                rel="next"
-                title="Historic"
-                aria-label="Historic"
-                className={styles.link}
-              >
-                Historic
-              </Link>
-            </li>
-          </ul>
-        </nav>
         <div className={styles.containerText}>
           <h1 className={styles.title}>Github username searcher</h1>
           <p className={styles.description}>Discover data from a GitHub user</p>
@@ -44,14 +17,14 @@ export default function Home({ searchParams }: { searchParams: SearchParams }) {
       </header>
       <main>
         <Form initialValue={searchParams.q} />
-        <Searcher
+        {/*<Searcher
           username={searchParams.q ? searchParams.q : "samuel-amaro"}
           pageIndex={
             !isNaN(parseInt(pageInd)) && parseInt(pageInd) >= 1
               ? parseInt(pageInd)
               : 1
           }
-        />
+        />*/}
       </main>
     </div>
   );
