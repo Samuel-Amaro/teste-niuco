d6328ae22c46e8e166dfe8986c114d1adef68212
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-09-17 15:49:32 -0300

    add logica para ordenação das pesquisas

--- src/components/Historic/index.tsx
+++ src/components/Historic/index.tsx
@@ -9,29 +9,35 @@ export default function Historic() {
   return (
     historicContext.historic.length > 0 && (
       <ul className={styles.list}>
-        {historicContext.historic.map((hist) => {
-          const newDate = new Date(hist.timestamp);
-          return (
-            <li key={hist.id} className={styles.listItem}>
-              <div className={styles.card}>
-                <span className={styles.cardIcon}>🔍</span>
-                <p
-                  className={styles.timestamp}
-                >{`${newDate.getMonth() + 1}/${newDate.getDate()}/${newDate.getFullYear()} ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}`}</p>
-                <Link
-                  href={`/?q=${hist.term}`}
-                  target="_self"
-                  rel="nex"
-                  title="Go to home search the term again"
-                  aria-label="Go to home search the term again"
-                  className={styles.cardLink}
-                >
-                  {hist.term}
-                </Link>
-              </div>
-            </li>
-          );
-        })}
+        {historicContext.historic
+          .sort((a, b) => {
+            const newDateA = new Date(a.timestamp);
+            const newDateB = new Date(b.timestamp);
+            return newDateA.getTime() - newDateB.getTime();
+          })
+          .map((hist) => {
+            const newDate = new Date(hist.timestamp);
+            return (
+              <li key={hist.id} className={styles.listItem}>
+                <div className={styles.card}>
+                  <span className={styles.cardIcon}>🔍</span>
+                  <p className={styles.timestamp}>{`${
+                    newDate.getMonth() + 1
+                  }/${newDate.getDate()}/${newDate.getFullYear()} ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}`}</p>
+                  <Link
+                    href={`/?q=${hist.term}`}
+                    target="_self"
+                    rel="nex"
+                    title="Go to home search the term again"
+                    aria-label="Go to home search the term again"
+                    className={styles.cardLink}
+                  >
+                    {hist.term}
+                  </Link>
+                </div>
+              </li>
+            );
+          })}
       </ul>
     )
   );
