7ec49d464e5773394f638661cba7f1ee8802c183
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-09-17 14:29:28 -0300

    add class e formatação data e hora

--- src/components/Historic/index.tsx
+++ src/components/Historic/index.tsx
@@ -2,28 +2,36 @@
 
 import { useHistoricContext } from "@/context/HistoricContext";
 import Link from "next/link";
+import styles from "./styles.module.css";
 
 export default function Historic() {
   const historicContext = useHistoricContext();
   return (
     historicContext.historic.length > 0 && (
-      <ul>
-        {historicContext.historic.map((hist) => (
-          <li key={hist.id}>
-            <div>
-              <p>{hist.timestamp}</p>
-              <Link
-                href={`/?q=${hist.term}`}
-                target="_self"
-                rel="nex"
-                title="Go to home search the term again"
-                aria-label="Go to home search the term again"
-              >
-                {hist.term}
-              </Link>
-            </div>
-          </li>
-        ))}
+      <ul className={styles.list}>
+        {historicContext.historic.map((hist) => {
+          const newDate = new Date(hist.timestamp);
+          return (
+            <li key={hist.id} className={styles.listItem}>
+              <div className={styles.card}>
+                <span className={styles.cardIcon}>🔍</span>
+                <p
+                  className={styles.timestamp}
+                >{`${newDate.getMonth() + 1}/${newDate.getDate()}/${newDate.getFullYear()} ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}`}</p>
+                <Link
+                  href={`/?q=${hist.term}`}
+                  target="_self"
+                  rel="nex"
+                  title="Go to home search the term again"
+                  aria-label="Go to home search the term again"
+                  className={styles.cardLink}
+                >
+                  {hist.term}
+                </Link>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     )
   );
